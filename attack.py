0cf37478ab4f15de4c11a3bdaa946b1da8a5ea9c
Online Hacking <45653647+OnlineHacKing@users.noreply.github.com>
2021-06-26 10:23:54 +0530

    Update attack.py

--- attack.py
+++ attack.py
@@ -108,8 +108,8 @@ DDDDDDDDDDDDD      DDDDDDDDDDDDD           ooooooooooo     sssssssssss
      +--------------------------------------+  
 """
 print '\033[96m    Ae brother after hacking a weed we crash site '
-print ''
-print '\033[93m    Stop DDos Attack : 
+
+print '\033[93m    Stop DDos Attack : CTRL + Z '
 print ''
 print('''\033[92m ---------------------------------------------------''')
 
